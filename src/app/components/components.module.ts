ecad16a2d40180839171bb5121bd75a54e78b04f
Daniel Hernandez <dhernandez@WHOU-9408-WKS.attlocal.net>
2017-12-02 19:09:03 -0600

    add markup, style to navbar component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,5 +1,13 @@
+// ANGULAR
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatMenuModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 // COMPONENTS
 import { NavbarComponent } from './navbar';
 
@@ -12,7 +20,12 @@ const components = [
   entryComponents: [],
   exports: components,
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule
   ]
 })
 export class ComponentsModule { }
